1afe5a8b75e0c19e0a7e44e4b487a2594984e560
Taran Rickard <taranteachestech@gmail.com>
2023-11-07 15:36:45 -0500

    productID route

--- back-end/src/Products/index.mjs
+++ back-end/src/Products/index.mjs
@@ -1,11 +1,15 @@
 import { Statement } from "/opt/nodejs/dynamoDB.mjs";
 
 export const rootHandler = async (event) => {
-  const result = await Statement('SELECT * FROM products')
+  const result = (event.pathParameters && ('id' in event.pathParameters)) ? 
+    Statement(`SELECT name, id, in_stock, nominal_dimensions FROM products WHERE id='${event.pathParameters.id}'`).then(data => data[0]) :
+    Statement('SELECT name, id, in_stock FROM products WHERE in_stock > 0')
+
+  const data = await result
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(result),
+    body: JSON.stringify(data),
   };
   return response;
 };
