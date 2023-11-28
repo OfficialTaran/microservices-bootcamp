492f1a5b0fe61d33cc1ae264bfeb13d8241d042e
Taran Rickard <taranteachestech@gmail.com>
2023-11-27 20:58:29 -0500

    actions vs mutations

--- front-end/src/components/Products/Products.vue
+++ front-end/src/components/Products/Products.vue
@@ -7,10 +7,18 @@
 
   export default {
     name: 'ProductsMain',
+    async created () {
+      await this.load()
+    },
     computed: {
       ...mapState({
         products: state => state.products.product_list
       })
+    },
+    methods: {
+      async load () {
+        await this.$store.dispatch('products/listProducts')
+      }
     }
   }
 </script>
--- front-end/src/store/modules/products.js
+++ front-end/src/store/modules/products.js
@@ -1,8 +1,30 @@
+import { makeAPICall } from "@utils/api.js"
+
 const state = {
   product_list: []
 }
 
+// actions can be async but can't update state
+const actions = {
+  async listProducts ({ commit }) {
+    const products = await makeAPICall({ route: '/api/products' })
+      .then(res => res.data)
+      .catch(err => console.error(err))
+
+    commit('setProductList', { products })
+  }
+}
+
+// mutations must be sync but can update state
+const mutations = {
+  setProductList ( state, { products }) {
+    state.product_list = products
+  }
+}
+
 export default {
   namespaced: true,
-  state
+  state,
+  actions,
+  mutations
 }
