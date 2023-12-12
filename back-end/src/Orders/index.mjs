8d123a3a192c0268210e59d8febd3db9b8712292
Taran Rickard <taranteachestech@gmail.com>
2023-12-11 20:09:43 -0500

    service defined properties

--- back-end/src/Orders/index.mjs
+++ back-end/src/Orders/index.mjs
@@ -1,10 +1,29 @@
 export const rootHandler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      msg: "hello world, I'm the first local function of the course!"
-    }),
-  };
-  return response;
+
+  const user_id = '1'
+  const body = JSON.parse(event.body || '{}')
+
+  return handlers[event.httpMethod]({ body, user_id })
 };
+
+const handlers = {
+  POST: async ({ body = null, user_id }) => {
+    const date_now = new Date()
+
+    const defined_data = {
+      state: 'needs_picked',
+      staging_location: 'none',
+      date_created: date_now.toISOString(),
+      customer: user_id
+    }
+
+    const data = { ...body, ...defined_data }
+
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  }
+}
   
